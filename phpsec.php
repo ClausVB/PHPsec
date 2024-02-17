ddbc3fa943ff89737d61c743d144266de1681449
ClausVB <159058140+ClausVB@users.noreply.github.com>
2024-02-17 17:21:35 +0100

    Second commit: Status BETA
    I added two working examples. There is a short "HowTo" with RegEx and all.

--- phpsec.php
+++ phpsec.php
@@ -2,6 +2,7 @@
 
 // Important constants
 define('PHPSEC_SERVER_IP_ADDRESS', $_SERVER['SERVER_ADDR']);
+define('PHPSEC_DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
 
 /**
  * PHPsec is a simple security class for PHP8.1 or later
@@ -16,13 +17,31 @@ class phpSec
 
 	/**
 	 * Creates all necessary properties and UNSET all SuperGlobals
-	 * @param $superglobal: lowercase 'get' or 'post'
+	 * which = which_type_of_superglobal, key_names = superglobal_key_names
+	 * @param $which_type: Lowercase 'get', 'request' or 'post'
+	 * @param $key_names: Every SuperGlobal is an array. All SuperGlobals have keys with names
+	 * like "script.php?x=99", name = x
 	**/
-	public function __construct(protected string $superglobal, protected array $api)
+	public function __construct(protected string $which_type, protected array $key_names)
 	{
-		foreach ($_GET as $key => $value)
+		switch ($which_type)
 		{
-			if (in_array($key, $this->api))
+			case 'get':
+				$superglobal = $_GET;
+			break;
+			case 'post':
+				$superglobal = $_POST;
+			break;
+			case 'request':
+				$superglobal = $_REQUEST;
+			break;
+			default:
+				exit('Critical error: <b>$which_type</b> is not set! $which_type: Lowercase "get", "request" or "post"');
+		}
+
+		foreach ($superglobal as $key => $value)
+		{
+			if (in_array($key, $this->key_names))
 			{
 				$this->getRaw[$key] = $value;
 			}
@@ -32,7 +51,24 @@ class phpSec
 			}
 		}
 
+		unset($_SERVER);
 		unset($_GET);
+		unset($_POST);
+		unset($_FILES);
+		unset($_COOKIE);
+		unset($_SESSION);
+		unset($_REQUEST);
+		unset($_ENV);
+
+
+		// !!! This must be in an IF in a future version ... if HARD_SECURTY === false then UNSET !!!
+
+		// PHP 8.1: https://php.watch/versions/8.1/GLOBALS-restrictions
+		// unset($GLOBALS) => Fatal error: $GLOBALS can only be modified using (...)
+		foreach (array_keys($GLOBALS) as $key)
+		{
+			unset($GLOBALS[$key]);
+		}
 	}
 
 	/**
@@ -45,6 +81,43 @@ class phpSec
 		if (is_int($this->number))
 		{
 			return $this->number;
+		}
+		else
+		{
+			// ELSE will throw an expception like:
+			// Fatal error: Uncaught TypeError: Cannot assign string to property phpSec::$number of type int
+			return false;
+		}
+	}
+
+	/**
+	 * getChars
+	**/
+	public function getChars($string)
+	{
+		if (is_string($string))
+		{
+			preg_replace('/[^[\w]/', '', $modified_string);
+			return $modified_string;
+		}
+		else
+		{
+			// ELSE will throw an expception like:
+			// Fatal error: Uncaught TypeError: Cannot assign string to property phpSec::$number of type int
+			return false;
+		}
+	}
+
+	/**
+	 * getAlphanumericalAndSpace /^[:alnum:][:space:]]/
+	**/
+	public function getAlphanumericalAndSpace($string)
+	{
+		if (is_string($string))
+		{
+			// CVB: ^ = NOT | Therefore every char is replaced which NOT matches (ASCII)Characters or space
+			preg_replace('/[^[:alnum:][:space:]]/', '', $modified_string);
+			return $modified_string;
 		}
 		else
 		{
--- test_and_docs_phpsec.php
+++ test_and_docs_phpsec.php
@@ -1,44 +1,47 @@
 <?php
 
-/**
- * $_GET: Array
- * (
- *     [test] => 1
- *     [name] => Daniel
- *     [age] => 99
- *     [some_evil_attack] => php://filter/convert.iconv.UTF8.CSISO2022KR|convert.base64-encode|convert.iconv.UTF8.UTF7
- * )
- *
- * GETRAW: array(3) {
- *   ["test"]=>
- *   string(1) "1"
- *   ["name"]=>
- *   string(6) "Daniel"
- *   ["age"]=>
- *   string(2) "99"
- * }
-**/
+
 
 if (!empty($_GET) and is_array($_GET))
 {
-	echo '<pre><a href="https://www.php.net/manual/de/language.variables.superglobals.php" target="_blank">SuperGlobal</a> $_GET: ' . PHP_EOL;
-	print_r($_GET);
-	echo '</pre>';
+	/*
+		echo '<pre><a href="https://www.php.net/manual/de/language.variables.superglobals.php" target="_blank">SuperGlobals</a> $_GET: ' . PHP_EOL;
+		print_r($_GET);
+		echo '</pre>';
+	*/
 }
 else
 {
-	exit('<h1>How to use correctly</h1><p>Call this file 3 params like: ' . basename(__FILE__) . '?test=1&name=Daniel&age=99</p>');
+	exit('<h1>How to use correctly</h1><p>Call this file with 3 params like: ' . basename(__FILE__) . '?test=1&name=Daniel&age=42</p><p>Example: <a href="' . basename(__FILE__) . '?test=1&name=Daniel&age=42">Test with params</a></p>');
 }
 
-require_once(dirname(__FILE__) . '/phpsec.php');
+require_once dirname(__FILE__) . '/phpsec.php';
 
 /**
- * The class "PHPsec" deletes all values
+ * The class "PHPsec" creates an API between your script and the outside world.
+ * It deletes all values/SuperGlobals like $_GET, $_POST and so on.
  * https://www.php.net/manual/de/language.variables.superglobals.php
 **/
-$api = array('test', 'name', 'age');
-$phpsec = new phpSec('get', $api);
+$superglobal_names = array('test', 'name', 'age');
+$phpsec = new phpSec('get', $superglobal_names);
 
-$test_number = $phpsec->isInt('5');
+preg_match('/^([1-5][0-9]|60|61|62)$/', $phpsec->getRaw['age'], $regex_matches);
+$tested_age = ($regex_matches[0] == $phpsec->getRaw['age'])
+	? $phpsec->isInt($phpsec->getRaw['age'])
+	: exit('Param "age" must be between 10 and 62!');
 
-echo '<pre>GETRAW: '; var_dump($phpsec->getRaw); echo '</pre>';
+
+/**
+ * Another short example: How to handle a simple string
+ * Name: Daniel, Michelle, Marc-Oliver or Jörg
+**/ 
+$name_htmlentities = htmlentities($phpsec->getRaw['name'], ENT_HTML5, 'UTF-8');
+
+preg_match('/^([\w&;-]+)$/', $name_htmlentities, $regex_name_matches);
+$tested_name = ($regex_name_matches[0] == $name_htmlentities)
+	? $name_htmlentities
+	: exit('Param "name" contains illegal characters!');
+
+//echo '<pre>GETRAW: '; var_dump($phpsec->getRaw); echo '</pre>';
+
+require_once 'test_and_docs_phpsec.htm';
